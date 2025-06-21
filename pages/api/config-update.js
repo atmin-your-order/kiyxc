e38cbd60b187ee3076f19dca011fe70e168e573a
atmin-your-order <sanjaya28051@gmail.com>
2025-06-22 00:51:41 +0700

    Update config-update.js

--- pages/api/config-update.js
+++ pages/api/config-update.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
   if (req.method !== 'POST')
     return res.status(405).json({ error: 'Method not allowed' })
 
-  const configPath = path.resolve('./config.json')
+  const configPath = path.resolve('../../config.json')
 
   try {
     const { domain, apikey, nodeName, nestName, eggName } = req.body
