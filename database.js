e4a285271bde83167c99fb077cb808719cf391e7
atmin-your-order <sanjaya28051@gmail.com>
2025-06-21 23:04:34 +0700

    Create database.js

---
+++ database.js
@@ -0,0 +1,19 @@
+const users = [
+  {
+    username: 'ikyyz',
+    password: 'powerfull',
+    role: 'admin'
+  },
+  {
+    username: 'iky',
+    password: 'iky123',
+    role: 'user'
+  },
+  {
+    username: 'amane',
+    password: 'amane01',
+    role: 'user'
+  }
+]
+
+export default users
