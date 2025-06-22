7b8978be2c517ef6b50e2e3a757c12b3535a766a
atmin-your-order <sanjaya28051@gmail.com>
2025-06-22 17:33:11 +0700

    Update config.js

--- pages/api/config.js
+++ pages/api/config.js
@@ -1,9 +1,7 @@
-// config.js
-
 module.exports = {
   panelURL: 'https://kenjapublic.digital-market.web.id',
   apiKey: 'ptla_WNQY5xBLAYF40mm1YuvPT2npMpsT8xcftCMdt3FXVKt',
-  nodeName: 'nodes',
+  nodeName: 'NODES', // sudah diperbaiki di atas
   nestName: 'untuk bot whatsapp',
   eggName: 'naofumi',
   dockerImage: 'ghcr.io/parkervcp/yolks:nodejs_18'
