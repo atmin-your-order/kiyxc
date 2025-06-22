f728c083461ef8092098ba6e92d1854895b76851
atmin-your-order <sanjaya28051@gmail.com>
2025-06-22 17:53:50 +0700

    Delete pages/api/config.js

--- pages/api/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  panelURL: 'https://kenjapublic.digital-market.web.id',
-  apiKey: 'ptla_WNQY5xBLAYF40mm1YuvPT2npMpsT8xcftCMdt3FXVKt',
-  nodeName: 'NODES', // sudah diperbaiki di atas
-  nestName: 'untuk bot whatsapp',
-  eggName: 'naofumi',
-  dockerImage: 'ghcr.io/parkervcp/yolks:nodejs_18'
-}
